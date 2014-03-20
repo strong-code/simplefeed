f115eb376c1915116559dcbdcc9db71d5a9d2264
clindsay107@gmail.com <clindsay107@gmail.com>
2014-03-20 12:03:57 -0700

    heroku fixesss

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-testuser = User.create(username: "TestUser", password: "password")
+testuser = User.create!(username: "TestUser", password: "password")
 
-testuser.feeds.create(url: "https://news.ycombinator.com/rss", title: "Hacker News RSS")
+testuser.feeds.create!(url: "https://news.ycombinator.com/rss", title: "Hacker News RSS")
