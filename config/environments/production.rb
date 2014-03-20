26dc38dceefffe0ce28143847acd03909b4014b2
clindsay107@gmail.com <clindsay107@gmail.com>
2014-03-20 10:11:22 -0700

    one more config change for heroku prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ SimpleFeed::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -53,6 +53,8 @@ SimpleFeed::Application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
