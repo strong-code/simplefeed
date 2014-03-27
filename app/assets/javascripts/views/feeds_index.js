a085bf89c31388b02d74ef5811801268b4912b6f
clindsay107@gmail.com <clindsay107@gmail.com>
2014-03-27 13:06:35 -0700

    incorrectly named variable for grabbing regex group in URL

--- app/assets/javascripts/views/feeds_index.js
+++ app/assets/javascripts/views/feeds_index.js
@@ -18,7 +18,8 @@ SimpleFeed.Views.FeedsIndex = Backbone.View.extend({
     if (currentFeed !== "" || currentFeed !== undefined) {
       var feedId = /\/feeds\/(\d*)$/.exec(currentFeed);
       if (feedId !== null && feedId !== null) {
-        $('#container-'+currentFeed[1]).toggleClass('selected-feed');
+        debugger
+        $('#container-'+feedId[1]).toggleClass('selected-feed');
       }
     }
     var renderedContent;
