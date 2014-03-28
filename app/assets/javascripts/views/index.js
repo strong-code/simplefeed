beeb7cf87acdd7ebdf1813cbbeac322b7fdb4744
clindsay107@gmail.com <clindsay107@gmail.com>
2014-03-28 14:54:51 -0700

    adding randomly generated messages on index view

--- app/assets/javascripts/views/index.js
+++ app/assets/javascripts/views/index.js
@@ -9,9 +9,34 @@ SimpleFeed.Views.Index = Backbone.View.extend({
 
     var renderedContent = this.template({
       feeds: this.collection,
-      totalUnread: totalUnread
+      totalUnread: totalUnread,
+      congratulations: this.congratulatory(),
+      motivational: this.motivational()
     });
     this.$el.html(renderedContent);
     return this;
+  },
+
+  congratulatory: function() {
+    var msgs = [
+      "Way to go champ",
+      "Speed reader over here",
+      "You little bookworm ;)",
+      "You have achieved inbox zero",
+      "Congratuliations"
+      ];
+      return msgs[Math.floor(Math.random() * msgs.length)];
+  },
+
+  motivational: function() {
+    var msgs = [
+      "Better get to reading...",
+      "Bro, do you even read?",
+      "It's like you aren't even trying",
+      "Try reading a bit faster",
+      "And it's wasting our bandwidth",
+      "And you're running up our hosting bill"
+    ];
+    return msgs[Math.floor(Math.random() * msgs.length)];
   }
 });
